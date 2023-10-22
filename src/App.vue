2fefc9787c7f2f5f833fd0e4e199a1aad47a03a3
konsavv <ksavvidi@hotmail.com>
2023-10-22 17:36:38 +0300

    Create navbar

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-</script>
-
 <template>
-  <nav>
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/favorites">Favorites</RouterLink>
-    <RouterLink to="/trash">Trash</RouterLink>
+  <nav class="flex flex-row gap-3 bg-gray-50 text-gray-800 justify-between border-b border-gray-100 md:px-10">
+    <div class="flex h-full">
+      <RouterLink class="p-3" active-class="border-b-8 border-pastelGreen-200 " to="/">Home</RouterLink>
+      <RouterLink class="p-3" active-class="border-b-8 border-pastelGreen-200 " to="/favorites">Favorites</RouterLink>
+      <RouterLink class="p-3" active-class="border-b-8 border-pastelGreen-200 " to="/trash">Trash</RouterLink>
+    </div>
+    <div>
+      <div class="justify-self-end flex gap-4 items-center justify-center">
+        <IconUpload class="cursor-pointer" />
+        <IconTrash class="cursor-pointer" />
+      </div>
+    </div>
   </nav>
   <RouterView />
 </template>
 
-
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import IconTrash from './components/icons/IconTrash.vue';
+import IconUpload from './components/icons/IconUpload.vue';
+</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,21 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      //where can i customize my colors instead of deafult colors
+      colors:{
+        pastelBlue:{
+          500: '#525E75'
+        },
+        pastelGreen:{
+          200: '#92BA92',
+          500: '#78938A',
+        },
+        pastelYellow:{
+          500: '#F1DDBF'
+        },
+      }
+    },
   },
   plugins: [],
 }
