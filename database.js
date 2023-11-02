f0991da6aed7932452ad6ad5a8a390f0cf4bece0
konsavv <ksavvidi@hotmail.com>
2023-11-02 08:58:39 +0200

    Set server and database

---
+++ database.js
@@ -0,0 +1,31 @@
+const sqlite3 = require("better-sqlite3");
+
+const DBSOURCE = "db.sqlite";
+
+const db = new sqlite3(DBSOURCE);
+
+//CREATE VIDEOFILE TABLE
+try {
+  db.prepare(`CREATE TABLE videofile
+              id INTEGER PRIMARY KEY AUTOINCREMENT,
+              filename text UNIQUE,
+              duration text,
+              is_fav integer,
+              is_del integer
+  `).run();
+} catch (error) {
+  console.log("error occured in create table step", error)
+}
+
+// INSERT A NEW VIDEO INTO THE TABLE
+function addToDb(filename, duration) {
+  const statement = db.prepare(`INSERT OR IGNORE INTO videofile (filename, duration, is_fav, is _del) VALUES (?, ?, ?, ?)`);
+
+  const info = statement.run(filename, duration, 0, 0);
+  return info.changes
+}
+
+module.exports = {
+  db,
+  addToDb,
+};
---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const path = require("path");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const multer = require("multer"); //Multer is a node. js middleware for handling multipart/form-data , which is primarily used for uploading files
+const { getVideoDurationInSeconds } = require("get-video-duration");
+const fs = require("fs");
+
+// SET STORAGE
+let storage = multer.diskStorage({
+  destination: function (_req, _file, callback) {
+    callback(null, "Videos/")
+  },
+  filename: function (_req, file, callback) {
+    callback(null, file.originalname);
+    // callback(null, file.fieldname + '-' + Date.now());
+  },
+});
+
+const uploads = multer({ storage: storage });
+const db = require("./database.js");
+
+const app = express();
+const port = 3000;
+
+const getVideoDuration = function (file, callback) {
+  getVideoDurationInSeconds("Video/" + file).then((duration) => {
+    db.addToDb(file, duration);
+    callback();
+  })
+}
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+//API FOR UPLOADING THE FILES
+app.post("/uploadVideo", uploads.single("videofile"), function (_req, _res) { })
