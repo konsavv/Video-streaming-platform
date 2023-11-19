0b48338021a150072b6741b57cb0fcb6bdbee879
konsavv <ksavvidi@hotmail.com>
2023-11-19 20:08:14 +0200

    Check in database if the table exists already

--- database.js
+++ database.js
@@ -3,16 +3,20 @@ const sqlite3 = require("sqlite3").verbose();
 const DBSOURCE = "db.sqlite";
 
 const db = new sqlite3.Database(DBSOURCE);
+const tableExists = db.get(`SELECT name FROM sqlite_master WHERE type='table' AND name='videofile'`);
 
 //CREATE VIDEOFILE TABLE
 try {
-  db.prepare(`CREATE TABLE videofile(
+  //CHECK IF THE TABLE ALREADY EXISTS IN DB
+  if (!tableExists) {
+    db.prepare(`CREATE TABLE videofile(
               id INTEGER PRIMARY KEY AUTOINCREMENT,
               filename text UNIQUE,
               duration text,
               is_fav integer,
               is_del integer
-  )`).run();
+              )`).run();
+  }
 } catch (error) {
   console.log("error occured in create table step", error)
 }
