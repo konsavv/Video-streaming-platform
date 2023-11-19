06a19aa369dbd669803b41c57c413afbe87e7477
konsavv <ksavvidi@hotmail.com>
2023-11-19 20:07:21 +0200

    Create a file to connect to the api

---
+++ frontend/src/services/api.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+const url = "http://localhost:3000/";
+
+export async function uploadFileApi(file: any ){
+  
+  let formdata = new FormData();
+
+  formdata.append('videofile', file);
+  const response = await axios.post(`${url}/uploadVideo`,formdata, {
+    headers:{
+      'Content-Type': 'multipart/form-data'
+    }
+  });
+  
+  return response
+}
