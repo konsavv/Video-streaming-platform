0dd92c1c2cf6e664b84dd01be7b90d371b3b0bfe
konsavv <ksavvidi@hotmail.com>
2023-12-02 13:58:16 +0200

    Add Apis in the store

--- frontend/src/stores/mainStore.ts
+++ frontend/src/stores/mainStore.ts
@@ -1,11 +1,33 @@
 import { defineStore } from "pinia";
 import { uploadFileApi } from "@/services/api";
 
-export const useMainStore = defineStore('mainstore', ()=>{
-  const uploadFile = (file:any)=>{
-    uploadFileApi(file).then((response:any)=>{
+export const useMainStore = defineStore('mainstore', () => {
+  const filelist = ref([]);
+  const favfilelist = ref([]);
+  const trashedfilelist = ref([]);
+
+  const uploadFile = (file: any) => {
+    uploadFileApi(file).then((response: any) => {
       console.log(response);
     })
   }
-  return{uploadFile}
+  const getFileListApi = () => {
+    getFileListApi().then((resp: any) => {
+      filelist.value = resp.data.filelist
+    })
+  }
+
+  const getFavFileListApi = () => {
+    getFavFileListApi().then((resp: any) => {
+      favfilelist.value = resp.data.filelist
+    })
+  }
+
+  const getTrashedFileListApi = () => {
+    getTrashedFileListApi().then((resp: any) => {
+      trashedfilelist.value = resp.data.filelist
+    })
+  }
+  return { uploadFile, getFileListApi, getFavFileListApi, getTrashedFileListApi }
 })
+
